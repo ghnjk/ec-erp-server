7d2698f05dcfd55ba8e0cc0de03a047ffe69d47a
jkguo <jkguo@tencent.com>
2024-03-10 00:18:28 +0800

    feat: update web

--- static/assets/shopSale.c0ea59e1.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,b as o,c as s}from"./index.111d7470.js";const a={name:"ShopSale"},t={src:"http://150.109.158.22:3000/d/e174cec8-2333-44df-8b09-26e116a93bf0/8ba447b7-2f12-5079-b9d3-59dd2e04ec62?orgId=1&from=now-30d&to=now&kiosk",style:{width:"100%",height:"100vh"}};function r(c,d,n,p,_,f){return o(),s("iframe",t,`
-  `)}var i=e(a,[["render",r],["__file","/Users/jkguo/workspace/ec-erp-static/src/pages/dashboard/shopSale.vue"]]);export{i as default};
