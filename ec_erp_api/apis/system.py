5e535247b3a340ea3d4ce0e5f259a53d9272de67
jkguo <jkguo@tencent.com>
2024-04-27 01:25:47 +0800

    update

--- ec_erp_api/apis/system.py
+++ ec_erp_api/apis/system.py
@@ -5,6 +5,8 @@
 @author: jkguo
 @create: 2024/2/24
 """
+import logging
+
 from ec_erp_api.common import request_util, response_util, request_context
 from flask import (
     Blueprint, session
@@ -18,28 +20,29 @@ system_apis = Blueprint('system', __name__)
 @system_apis.route('/login_user_with_token', methods=["POST"])
 @api_post_request()
 def login_user_with_token():
-    print("login_user_with_token")
+    logger = logging.getLogger("ACC")
+    logger.error("login_user_with_token")
     token = request_util.get_str_param("token")
     if token is None or token == "":
         return _get_login_user_info()
     else:
-        print(f"token: {token}")
+        logger.error(f"token: {token}")
         # 将token base64 decode
         token = codec_util.base64_decode(token)
-        print(f"token: {token}")
+        logger.error(f"token: {token}")
         # 将token字符串按照url query string格式转成dict
         token_dict = request_util.parse_url_query_string(token)
         user_name = token_dict.get("user_name")
         password = token_dict.get("password")
-        print(f"user_name: {user_name} password: {password}")
+        logger.error(f"user_name: {user_name} password: {password}")
         password = codec_util.calc_sha256(password)
         user = request_context.get_backend().get_user(user_name)
         if user is None:
-            print("user is None")
+            logger.error("user is None")
         else:
-            print(f"user is not None")
-            print(f"user name: {user.user_name}")
-            print(f"user: {user.password}")
+            logger.error(f"user is not None")
+            logger.error(f"user name: {user.user_name}")
+            logger.error(f"user: {user.password}")
         if user is None or user.is_delete or password != user.password:
             return response_util.pack_error_json_response(1002, "用户不存在或者密码异常")
         session["user_name"] = user.user_name
@@ -48,16 +51,17 @@ def login_user_with_token():
 
 
 @system_apis.route('/login_user', methods=["POST"])
+@api_post_request()
 def login_user():
     user_name = request_util.get_str_param("account")
     password = request_util.get_str_param("password")
     password = codec_util.calc_sha256(password)
     user = request_context.get_backend().get_user(user_name)
     if user is None or user.is_delete or password != user.password:
-        return response_util.pack_error_response(1002, "用户不存在或者密码异常")
+        return response_util.pack_error_json_response(1002, "用户不存在或者密码异常")
     session["user_name"] = user.user_name
     session["project_id"] = user.default_project_id
-    return get_login_user_info()
+    return _get_login_user_info()
 
 
 def _get_login_user_info():
