e24a42b57e56be72283eeba674be1f2c2bd2178e
jkguo <jkguo@tencent.com>
2025-06-22 08:51:07 +0800

    feat: 优化重启指令，支持多实例部署

--- restart.sh
+++ restart.sh
@@ -4,6 +4,15 @@ conda activate ec_erp_env
 
 mkdir -p logs
 
-ps aux | grep ec_erp_app.py | grep -v grep| grep python | awk '{print $2;}' | xargs kill -9
+listen_port=`grep listen_port conf/application.json | awk -F ":" '{print $2;}' | tr -d ","`
+procs=`netstat -nlpt | grep $listen_port | awk '{print $7;}'  | awk -F"/" '{print $1;}' `
+
+if [[ "$procs" != "" ]]
+then
+  echo "stopping process $procs"
+  echo "$procs" | xargs kill -9
+else
+  echo "No process found. not need stop."
+fi
 
 nohup python ec_erp_app.py 2>&1 1>>std.log &
